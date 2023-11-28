fb1dddc72b9d3a8bf6255d9170a957da77cd27a8
plasticgaming99 <plsplassub@gmail.com>
2023-11-28 20:38:20 +0900

    maybe first commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		//quit with error
+		os.Exit(1)
+	}
+	if os.Args[1] == "cd" {
+		chdir(os.Args[2])
+	}
+	if os.Args[1] == "chdir" {
+		chdir(os.Args[2])
+	}
+	if os.Args[1] == "echo" {
+		echo(os.Args[2])
+	}
+	if os.Args[1] == "yes" {
+		yes()
+	}
+	if os.Args[1] == "pwd" {
+		pwd()
+	}
+	if os.Args[1] == "ls" {
+		if len(os.Args) >= 3 {
+			ls(os.Args[2])
+		} else {
+			dir, err := os.Getwd()
+			if err != nil {
+				panic(err)
+			}
+			ls(dir)
+		}
+
+	}
+}
+
+func chdir(path string) {
+	os.Chdir(path)
+}
+
+func echo(text string) {
+	fmt.Println(text)
+}
+
+func yes() {
+	for {
+		fmt.Println("yes")
+	}
+}
+
+func ls(path string) {
+	files, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	for _, file := range files {
+		fmt.Print(file.Name() + "  ")
+	}
+	println()
+}
+
+func pwd() {
+	fmt.Println(os.Getwd())
+}
